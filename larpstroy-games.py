d84e5661ab290d856aaab84af2e76621c10273fc
52zagain52-blip <52zagain52@gmail.com>
2026-06-28 23:29:59 +0700

    Update larpstroy-games.py

--- larpstroy-games.py
+++ larpstroy-games.py
@@ -6,138 +6,34 @@ print("▓██▒    ▒████▄    ▓██ ▒ ██▒▓██▒
 print("▒██░    ▒██  ▀█▄  ▓██ ░▄█ ▒▒██▒   ░ ░ ▓██▄   ▒ ▓██░ ▒░▓██ ░▄█ ▒▒██▒░ ▓██▄   ▒██░  ▒██░")
 print("▒██░    ░██▄▄▄▄██ ▒██▀▀█▄  ░██░   ░  ▒   ██▒░ ▓██▓ ░ ▒██▀▀█▄  ░██░  ▒   ██▒▒██░  ▒██░")
 print("░██████▒ ▓█   ▓██▒░██▓ ▒██▒░██░   ░ ██████▒▒  ▒██▒ ░ ░██▓ ▒██▒░██░▒██████▒▒░██████░██████▒")
-x = int(input("..."))
 y = 0
-if x == 1:
-    if y != 0:    
-        zyx = int(input("выбери 1, 2 или 3"))
-        xyz = random.choice([1, 2, 3])
-        if xyz == zyx:
-            global balance
-            y = y + 10
-            print("you win")
+while True:
+    x = int(input("..."))
+    if x == 1:
+        if y != 0:    
+            zyx = int(input("выбери 1, 2 или 3"))
+            xyz = random.choice([1, 2, 3])
+            if xyz == zyx:
+                global balance
+                y = y + 10
+                print("you win")
+            else:
+                global balance
+                y = y - 5
+                print("ты проиграл")
         else:
+            print("у вас нет денег")
+    elif x == 2:
+        if y == 0:
             global balance
-            y = y - 5
-            print("ты проиграл")
-    else:
-        print("у вас нет денег")
-elif x == 2:
-    if y == 0:
-        global balance
-        y = y + 5
-        print("вы успешно поплнили баланс на 5р")
-    else:
-        print("у вас достаточно р чтоб играть")
-else:
-    print("1 - игра")
-    print("2 - поплнить бесплатно счет (если нет денег)")
-vvv = int(input("..."))
-if vvv == 1:
-    if y != 0:    
-        zyx = int(input("выбери 1, 2 или 3"))
-        xyz = random.choice([1, 2, 3])
-        if xyz == zyx:
-            global balance
-            y = y + 10
-            print("you win")
+            y = y + 5
+            print("вы успешно поплнили баланс на 5р")
         else:
-            global balance
-            y = y - 5
-            print("ты проиграл")
+            print("у вас достаточно р чтоб играть")
+    elif x == 3:
+        print(y)
     else:
-        print("у вас нет денег")
-elif vvv == 2:
-    if y == 0:
-        global balance
-        y = y + 5
-        print("вы успешно поплнили баланс на 5р")
-    else:
-        print("у вас достаточно р чтоб играть")
-else:
-    print("1 - игра")
-    print("2 - поплнить бесплатно счет (если нет денег)")
-vvc = int(input("..."))
-if vvc == 1:
-    if y != 0:    
-        zyx = int(input("выбери 1, 2 или 3"))
-        xyz = random.choice([1, 2, 3])
-        if xyz == zyx:
-            global balance
-            y = y + 10
-            print("you win")
-        else:
-            global balance
-            y = y - 5
-            print("ты проиграл")
-    else:
-        print("у вас нет денег")
-elif vvc == 1: 
-    if y != 0:    
-        zyx = int(input("выбери 1, 2 или 3"))
-        xyz = random.choice([1, 2, 3])
-        if xyz == zyx:
-            global balance
-            y = y + 10
-            print("you win")
-        else:
-            global balance
-            y = y - 5
-            print("ты проиграл")
-    else:
-        print("у вас нет денег")
-vvc = int(input("..."))
-if vvc == 1:
-    if y != 0:    
-        zyx = int(input("выбери 1, 2 или 3"))
-        xyz = random.choice([1, 2, 3])
-        if xyz == zyx:
-            global balance
-            y = y + 10
-            print("you win")
-        else:
-            global balance
-            y = y - 5
-            print("ты проиграл")
-    else:
-        print("у вас нет денег")
-elif vvc == 1:
-    if y != 0:    
-        zyx = int(input("выбери 1, 2 или 3"))
-        xyz = random.choice([1, 2, 3])
-        if xyz == zyx:
-            global balance
-            y = y + 10
-            print("you win")
-        else:
-            global balance
-            y = y - 5
-            print("ты проиграл")
-    else:
-        print("у вас нет денег")
-vvv = int(input("..."))
-if vvv == 1:
-    if y != 0:    
-        zyx = int(input("выбери 1, 2 или 3"))
-        xyz = random.choice([1, 2, 3])
-        if xyz == zyx:
-            global balance
-            y = y + 10
-            print("you win")
-        else:
-            global balance
-            y = y - 5
-            print("ты проиграл")
-    else:
-        print("у вас нет денег")
-elif vvv == 2:
-    if y == 0:
-        global balance
-        y = y + 5
-        print("вы успешно поплнили баланс на 5р")
-    else:
-        print("у вас достаточно р чтоб играть")
-else:
-    print("1 - игра")
-    print("2 - поплнить бесплатно счет (если нет денег)")
+        print("1 - игра")
+        print("2 - поплнить бесплатно счет (если нет денег)")
+        print("3 - какой счет у пользователя")
 
