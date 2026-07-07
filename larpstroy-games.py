651064402ad80db971e72f76da7a3cde13e5f84d
52zagain52-blip <52zagain52@gmail.com>
2026-07-07 08:40:30 +0700

    Update larpstroy-games.py

--- larpstroy-games.py
+++ larpstroy-games.py
@@ -63,8 +63,8 @@ if password_palyer == password:
             print("7 - сюжет(скоро)")
             print("8 - паспорт (скоро")
             print("9 - выход")
-elif x == 9:
-    break
+        elif x == 9:
+            break
         
 else:
     print("неправильный пароль (экстренный конец кода)")
